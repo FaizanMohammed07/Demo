3a076db24dc10e1bfe58af01ad5f5dda10ab42ee
faizanMohammed07 <aliffaizan1212@gmail.com>
2025-08-28 00:00:32 +0530

    first commit

---
+++ src/Demo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Demo() {
+  return (
+    <div>
+      <h1 className="pt-10">Hello, this is the Demo component!</h1>
+    </div>
+  );
+}
+
+export default Demo;
